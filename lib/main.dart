75af50d2ae5248da305177bb19ca2f16e826dc22
Ivan Huang <ivanhuang77@gmail.com>
2019-02-16 15:00:17 -0800

    Added Contact Form and firebase functionality

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,21 @@ class _PrayerKeeperState extends State<PrayerKeeper> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Prayer Keeper')
+        title: new Text('Prayer Keeper'),
+        backgroundColor: Colors.greenAccent[400],
       ),
       body: _buildBody(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){},
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute()),
+          );
+        },
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.green[400],
+        color: Colors.greenAccent[400],
         child: Container(height: 50.0),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
@@ -83,4 +89,78 @@ class Record {
   Record.fromSnapshot(DocumentSnapshot snapshot)
     : this.fromMap(snapshot.data, reference: snapshot.reference);
 }
+
+// ------------- SECOND ROUTE --------------------
+
+class SecondRoutePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Add Prayer Contact',
+      home: new SecondRoute(),
+    );
+  }
+}
+
+class SecondRoute extends StatefulWidget {
+  @override
+  _SecondRouteState createState() {
+    return _SecondRouteState();
+  }
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  final formKey = GlobalKey<FormState>();
+  String _name, _prayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Add Prayer Contact.")
+      ),
+      body: Card(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Name'
+                    ),
+                    onSaved: (input) => _name = input,
+                  ),
+//                  TextFormField(
+//                    decoration: InputDecoration(
+//                        labelText: 'Prayer'
+//                    ),
+//                    onSaved: (input) => _prayer = input,
+//                  ),
+                  RaisedButton(
+                    onPressed: (){
+                      _submit();
+                      Navigator.pop(context);
+                    },
+                    child: Text('Create Contact'),
+                  ),
+                ]
+            ),
+          ),
+        ),
+      )
+    );
+    }
+
+  void _submit() {
+    formKey.currentState.save();
+    print(_name);
+//    print(_prayer);
+    Firestore.instance.collection('people').document().setData({'name':_name});
+
+  }
+}
+
 

6140c6bd41c3296c8f3bfea559dbd5e49c20c8c6
Winston Lan <winston.chunhua.lan@gmail.com>
2019-02-16 09:42:20 -0800

    Implement PrayerKeeper Template Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new PrayerKeeperApp());
 
+final dummySnapshot = [
+  {"name": "Michael Scott"},
+  {"name": "Jim Halpert"},
+  {"name": "Pam Beasley"},
+];
+
 class PrayerKeeperApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       title: 'Prayer Keeper',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Prayer Keeper'),
-        ),
-      ),
+      home: new PrayerKeeper(),
     );
   }
 }
 
+class PrayerKeeper extends StatefulWidget {
+  @override
+  _PrayerKeeperState createState() {
+    return _PrayerKeeperState();
+  }
+}
+
+class _PrayerKeeperState extends State<PrayerKeeper> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Prayer Keeper')
+      ),
+    );
+  }
+}
